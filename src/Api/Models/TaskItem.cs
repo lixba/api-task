3848456df9c2963884f40f788b17640ed8421131
Luis Ixba <lixba@uv.mx>
2026-03-09 13:19:00 -0600

    correcione de codigo

--- src/Api/Models/TaskItem.cs
+++ src/Api/Models/TaskItem.cs
@@ -2,14 +2,14 @@ namespace Api.Models;
 public class TaskItem{
     public int Id{get;set;}
     public string Title{get;set;}
-    public string? Descripcion{get;set;}
+    public string Descripcion{get;set;}
     public bool IsCompleted{get;set;}
     public Priority Priority{get;set;}
     public DateTime CreatedAt {get;set;}
-    public DateTime? UpdatedAt{get;set}
+    public DateTime? UpdatedAt{get;set;}
 }
 public enum Priority{
-    Low =0;
-    Medium =1;
-    High=2;
+    Low =0,
+    Medium =1,
+    High=2,
 }
